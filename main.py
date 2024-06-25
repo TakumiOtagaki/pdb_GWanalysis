714c2a91ffa5d36fcead398c0a6a2badb2b009ba
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2024-06-25 18:16:25 +0900

    gw moved to modules dir, so I modified

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from Bio.PDB import PDBParser
+# from Bio.PDB import PDBParser
 import pandas as pd
 # import multiprocessing
 from multiprocessing import Pool
@@ -10,83 +10,52 @@ import glob
 # import ot
 from scipy.special import logsumexp
 
+from modules.gw import my_gromov_wasserstein_distance2, my_sinkhorn, check_convergence
+
 # NUM_CPU = multiprocessing.cpu_count()   
-NUM_CPU = 1
+NUM_CPU = 10
 SKIP_DOWNLOADING = True
 print(f"Number of CPUs: {NUM_CPU}")
 
-def parse_and_compute_distance_matrix(args):
-    pdb_file, output_directory = args
-    parser = PDBParser(QUIET=True)
-    structure = parser.get_structure('RNA', pdb_file)
-    pdb_file_wo_suffix = re.sub(r'^pdb', '', os.path.basename(pdb_file))
-    # もし RNA じゃなくて DNA なら、やめる
-    # output_file = os.path.join(output_directory, f"{os.path.basename(pdb_file).split('.')[0]}_{*}_{*}.csv")
-    # もし存在して、空じゃないのならやめる。
-    output_file_regex = os.path.join(output_directory, f"{os.path.basename(pdb_file_wo_suffix).split('.')[0]}_*.csv")
-    # return None
-    # if os.path.exists(output_file_regex):
-    if glob.glob(output_file_regex):
-        # print(f"File {output_file_regex.split('/')[-1].split('.csv')[0]} already exists and is not empty. Skipping...")
-        return None
-    print(f"Processing {pdb_file}...")
+# def parse_and_compute_distance_matrix(args):
+#     pdb_file, output_directory = args
+#     parser = PDBParser(QUIET=True)
+#     structure = parser.get_structure('RNA', pdb_file)
+#     pdb_file_wo_suffix = re.sub(r'^pdb', '', os.path.basename(pdb_file))
+#     # もし RNA じゃなくて DNA なら、やめる
+#     # output_file = os.path.join(output_directory, f"{os.path.basename(pdb_file).split('.')[0]}_{*}_{*}.csv")
+#     # もし存在して、空じゃないのならやめる。
+#     output_file_regex = os.path.join(output_directory, f"{os.path.basename(pdb_file_wo_suffix).split('.')[0]}_*.csv")
+#     # return None
+#     # if os.path.exists(output_file_regex):
+#     if glob.glob(output_file_regex):
+#         # print(f"File {output_file_regex.split('/')[-1].split('.csv')[0]} already exists and is not empty. Skipping...")
+#         return None
+#     print(f"Processing {pdb_file}...")
     
-    if not any(residue.get_resname() in ['A', 'C', 'G', 'U'] for model in structure for chain in model for residue in chain):
-        return None
-    for model in structure:
-        for chain in model:
-            atoms = [atom for residue in chain if residue.get_resname() in ['A', 'C', 'G', 'U']
-                     for atom in residue if atom.get_name() in ["P",  "C3'",  "O3'"]]
-            if atoms:
-                num_atoms = len(atoms)
-                dist_matrix = np.zeros((num_atoms, num_atoms))
-                for i in range(num_atoms):
-                    for j in range(i + 1, num_atoms):
-                        distance = atoms[i].coord - atoms[j].coord
-                        dist_matrix[i, j] = dist_matrix[j, i] = np.sqrt(np.sum(distance**2))
+#     if not any(residue.get_resname() in ['A', 'C', 'G', 'U'] for model in structure for chain in model for residue in chain):
+#         return None
+#     for model in structure:
+#         for chain in model:
+#             atoms = [atom for residue in chain if residue.get_resname() in ['A', 'C', 'G', 'U']
+#                      for atom in residue if atom.get_name() in ["P",  "C3'",  "O3'"]]
+#             if atoms:
+#                 num_atoms = len(atoms)
+#                 dist_matrix = np.zeros((num_atoms, num_atoms))
+#                 for i in range(num_atoms):
+#                     for j in range(i + 1, num_atoms):
+#                         distance = atoms[i].coord - atoms[j].coord
+#                         dist_matrix[i, j] = dist_matrix[j, i] = np.sqrt(np.sum(distance**2))
                 
-                # Save the distance matrix to a CSV file
-                # pdb_file がたまに pdb から始まっていることがあるのでその場合は suffix から"pdb"という文字列を取り除く。
-                # base = f"{os.path.basename(pdb_file).split('.')[0]}_{model.id}_{chain.id}"
-                base = f"{os.path.basename(pdb_file_wo_suffix).split('.')[0].replace('pdb', '')}_{model.id}_{chain.id}"
-                output_file = os.path.join(output_directory, f"{base}.csv")
-                pd.DataFrame(dist_matrix).to_csv(output_file, index=False)
-        break # Only process the first model
-    return None
-
-def my_sinkhorn(a, b, C, epsilon, max_iter=100000):
-    n, m = C.shape
-    f = np.ones(n)
-    g = np.ones(m)
-    for t in range(max_iter):
-        if t % 1000 == 0:
-            print(f"Iteration {t} / {max_iter}")
-        # f は g から
-        f = epsilon * np.log(a) - epsilon * logsumexp((-C + g.reshape(1, m)) / epsilon, axis=1) 
-        # g は f から
-        #         g = -epsilon * torch.logsumexp((-C + f.reshape(n, 1)) / epsilon, dim=0) + epsilon * torch.log(q)
-        g = epsilon * np.log(b) - epsilon * logsumexp((-C + f.reshape(n, 1)) / epsilon, axis=0)
-    P = np.exp((-C + f.reshape(n, 1) + g.reshape(1, m)) / epsilon)
-    return P
-
-def check_convergence(P, C, small_value = 1e-3):
-    return np.linalg.norm(P.sum(axis=1) - 1) < small_value and np.linalg.norm(P.sum(axis=0) - 1) < small_value
-
-def my_gromov_wasserstein_distance2(matrix1, matrix2, max_iter=100000, epsilon=1e-4, lambda_=1e1):
-    n, m = matrix1.shape[0], matrix2.shape[0]
-    a = np.ones(n) / n
-    b = np.ones(m) / m
-
-    P = np.ones((n, m)) / (n * m)
-    for t in range(max_iter):
-        C = -4 * matrix1 @ P @ matrix2.T + (epsilon - lambda_) * np.log(P)
-        P = my_sinkhorn(a, b, C, lambda_)
-        if check_convergence(P, C, epsilon):
-            break
-    return np.sum(P * C)
-    
+#                 # Save the distance matrix to a CSV file
+#                 # pdb_file がたまに pdb から始まっていることがあるのでその場合は suffix から"pdb"という文字列を取り除く。
+#                 # base = f"{os.path.basename(pdb_file).split('.')[0]}_{model.id}_{chain.id}"
+#                 base = f"{os.path.basename(pdb_file_wo_suffix).split('.')[0].replace('pdb', '')}_{model.id}_{chain.id}"
+#                 output_file = os.path.join(output_directory, f"{base}.csv")
+#                 pd.DataFrame(dist_matrix).to_csv(output_file, index=False)
+#         break # Only process the first model
+#     return None
         
-
 
 
         
@@ -111,9 +80,9 @@ if __name__ == "__main__":
 
     pdb_files = [os.path.join(pdb_directory, f) for f in os.listdir(pdb_directory) if f.endswith('.ent')]
     pdb_ids = [os.path.basename(f).split('.')[0] for f in pdb_files]
-    if not SKIP_DOWNLOADING:
-        with Pool() as pool:
-            pool.map(parse_and_compute_distance_matrix, [(pdb_file, output_directory) for pdb_file in pdb_files])
+    # if not SKIP_DOWNLOADING:
+    #     with Pool() as pool:
+    #         pool.map(parse_and_compute_distance_matrix, [(pdb_file, output_directory) for pdb_file in pdb_files])
 
     # List all distance matrix files
     matrix_files = [os.path.join(output_directory, f) for f in os.listdir(output_directory) if f.endswith('.csv')]
